dff6e8b2b7fea40fa9d7f9e12a6c7b3e504a9735
Chinna Kotla Hemalatha <nchlr1972@gmail.com>
2020-09-12 22:20:27 +0530

    initial commit

---
+++ app/src/main/java/com/example/retrofitapp/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.retrofitapp;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.List;
+
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MainActivity extends AppCompatActivity {
+    RecyclerView rv;
+    MyAdapter adapter;
+    ProgressDialog progressDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_main );
+        rv = findViewById( R.id.rec );
+        progressDialog = new ProgressDialog( MainActivity.this );
+        progressDialog.setMessage( "Loading...." );
+        progressDialog.show();
+        /*Create handle for the RetrofitInstance interface*/
+        GetDataService service = RetrofitClientInstance.getRetrofitInstance().create( GetDataService.class );
+
+        Call<List<Repo>> call = service.getAllPhotos();
+        call.enqueue( new Callback<List<Repo>>() {
+            @Override
+            public void onResponse(Call<List<Repo>> call , Response<List<Repo>> response) {
+                progressDialog.dismiss();
+                generateDataList( response.body() );
+            }
+
+            @Override
+            public void onFailure(Call<List<Repo>> call , Throwable t) {
+                progressDialog.dismiss();
+                Toast.makeText( MainActivity.this , "Something went wrong...Please try later!" , Toast.LENGTH_SHORT ).show();
+            }
+        } );
+    }
+
+    /*Method to generate List of data using RecyclerView with custom adapter*/
+    private void generateDataList(List<Repo> photoList) {
+        adapter = new MyAdapter( photoList , this );
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager( MainActivity.this );
+        rv.setLayoutManager( layoutManager );
+        rv.setAdapter( adapter );
+    }
+}
